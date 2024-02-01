c47c9fb83c333f44cb2a3b39ff595cbbec9a5b2e
Rama <rmdhnprmd@gmail.com>
2024-02-01 21:44:05 +0700

    create crud for category course

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -16,6 +17,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       id: params.courseId,
     },
   });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    }
+  })
+
 
   if (!course) redirect("/");
 
@@ -60,6 +68,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <ImageForm 
             initialData={course}
             courseId={course.id}
+          />
+          <CategoryForm 
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id
+            }))}
           />
         </div>
       </div>
--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -1,35 +1,31 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Check, ChevronsUpDown } from "lucide-react"
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 
 interface ComboboxProps {
-  options: {label: string; value: string}[];
+  options: { label: string; value: string }[];
   value?: string;
   onChange: (value: string) => void;
 }
 
-export const Combobox = ({
-  options,
-  value,
-  onChange
-}: ComboboxProps) => {
-  const [open, setOpen] = React.useState(false)
+export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
+  const [open, setOpen] = React.useState(false);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -38,7 +34,7 @@ export const Combobox = ({
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-full justify-between"
         >
           {value
             ? options.find((option) => option.value === value)?.label
@@ -46,7 +42,7 @@ export const Combobox = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput placeholder="Search option..." />
           <CommandEmpty>No option found.</CommandEmpty>
@@ -55,8 +51,8 @@ export const Combobox = ({
               <CommandItem
                 key={option.value}
                 onSelect={() => {
-                  onChange(option.value === value ? "" : option.value)
-                  setOpen(false)
+                  onChange(option.value === value ? "" : option.value);
+                  setOpen(false);
                 }}
               >
                 <Check
@@ -72,5 +68,5 @@ export const Combobox = ({
         </Command>
       </PopoverContent>
     </Popover>
-  )
-}
+  );
+};
