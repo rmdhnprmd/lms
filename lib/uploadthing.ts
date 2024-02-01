d00d9baab1e59fdb65d71610150e9b179e1048df
Rama <rmdhnprmd@gmail.com>
2024-02-01 17:18:52 +0700

    change some configuration

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,17 @@
-import {generateComponents} from "@uploadthing/react";
+// import {generateComponents} from "@uploadthing/react";
 
+// import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
+// export const {UploadButton, UploadDropzone, Uploader} = generateComponents<OurFileRouter>();
+
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+  generateUploader,
+} from "@uploadthing/react";
+ 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
-
-export const {UploadButton, UploadDropzone, Uploader} = generateComponents<OurFileRouter>();
+ 
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const Uploader = generateUploader<OurFileRouter>();
