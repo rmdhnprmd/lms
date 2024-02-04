bf8bbc24e59c045d7d02a8571abe1bca555285ec
Rama <rmdhnprmd@gmail.com>
2024-02-04 22:50:03 +0700

    change position toaster

--- components/providers/toaster-provider.tsx
+++ components/providers/toaster-provider.tsx
@@ -3,5 +3,5 @@
 import { Toaster } from "react-hot-toast";
 
 export const ToastProvider = () => {
-  return <Toaster position="bottom-right" reverseOrder/>;
+  return <Toaster position="top-right" reverseOrder/>;
 };
