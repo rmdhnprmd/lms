75e2835c2ffe3cde014d90c7b9f90a80790d92e3
Rama <rmdhnprmd@gmail.com>
2024-02-08 23:56:50 +0700

    create chapter video form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,11 +1,13 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({
   params,
@@ -61,22 +63,50 @@ const ChapterIdPage = async ({
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="space-y-4">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">
+                Customize your chapter
+              </h2>
+            </div>
+            <ChapterTitleForm 
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm 
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl">
+                Access Settings
+              </h2>
+            </div>
+            <ChapterAccessForm 
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+        </div>
         <div>
           <div className="flex items-center gap-x-2">
-            <IconBadge icon={LayoutDashboard} />
+            <IconBadge icon={Video} />
             <h2 className="text-xl">
-              Customize your chapter
+              Add a video
             </h2>
           </div>
-          <ChapterTitleForm 
+          <ChapterVideoForm 
             initialData={chapter}
-            courseId={params.courseId}
             chapterId={params.chapterId}
-          />
-          <ChapterDescriptionForm 
-            initialData={chapter}
             courseId={params.courseId}
-            chapterId={params.chapterId}
           />
         </div>
       </div>
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,13 @@
-import { db } from "@/lib/db";
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+const { video } = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET,
+})
 
 export async function PATCH(
   req: Request,
@@ -8,7 +15,7 @@ export async function PATCH(
 ) {
   try {
     const { userId } = auth();
-    const {isPublished, ...values} = await req.json();
+    const { isPublished, ...values } = await req.json();
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -32,8 +39,40 @@ export async function PATCH(
       },
       data: {
         ...values,
+      },
+    });
+
+    // Handle create video 
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if(existingMuxData) {
+        await video.assets.delete(existingMuxData.assetId)
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          }
+        });
       }
-    })
+
+      const asset = await video.assets.create({
+        input: values.videoUrl,
+        playback_policy: ["public"],
+        test: false,
+      })
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        }
+      })
+    }
 
     return NextResponse.json(chapter);
   } catch (error) {
